ebebb3c4dd3d79e1f90a719602f30ad3a8989e0e
@Ajinath Tangade <93082482+AjinathTangade@users.noreply.github.com>
2023-11-02 18:41:47 +0000

    product data

---
+++ assets/js/product.js
@@ -0,0 +1,318 @@
+var products = [
+  {
+    id: 1,
+    title: "Classic Wayfarer Sunglasses",
+    description: "Timeless style with UV protection and polarized lenses.",
+    price: 99.99,
+    size: "Medium",
+    image: "wayfarer.jpg",
+  },
+  {
+    id: 2,
+    title: "Retro Round Glasses",
+    description: "Vintage-inspired round frames with clear lenses.",
+    price: 49.99,
+    size: "Small",
+    image: "roundglasses.jpg",
+  },
+  {
+    id: 3,
+    title: "Designer Reading Glasses",
+    description: "High-quality reading glasses with a stylish frame.",
+    price: 79.99,
+    size: "Small",
+    image: "readingglasses.jpg",
+  },
+  {
+    id: 4,
+    title: "Sports Performance Eyewear",
+    description:
+      "Durable sports glasses with shatterproof lenses for active lifestyles.",
+    price: 69.99,
+    size: "Large",
+    image: "sportseyewear.jpg",
+  },
+  {
+    id: 5,
+    title: "Fashionable Cat-Eye Glasses",
+    description: "Elegant cat-eye frames for a chic look.",
+    price: 119.99,
+    size: "Medium",
+    image: "cateyeglasses.jpg",
+  },
+  {
+    id: 6,
+    title: "Prescription Aviator Glasses",
+    description: "Classic aviator style with prescription lenses.",
+    price: 129.99,
+    size: "Large",
+    image: "aviatorglasses.jpg",
+  },
+  {
+    id: 7,
+    title: "Kids' Fun and Colorful Glasses",
+    description:
+      "Colorful and playful glasses for children with impact-resistant lenses.",
+    price: 39.99,
+    size: "Kids",
+    image: "kidsglasses.jpg",
+  },
+  {
+    id: 8,
+    title: "Fashion Oversized Frames",
+    description: "Oversized frames for a bold and trendy look.",
+    price: 89.99,
+    size: "Large",
+    image: "oversizedframes.jpg",
+  },
+  {
+    id: 9,
+    title: "Anti-Blue Light Computer Glasses",
+    description:
+      "Computer glasses to reduce eye strain from screens with anti-blue light coating.",
+    price: 59.99,
+    size: "Medium",
+    image: "computer glasses.jpg",
+  },
+  {
+    id: 10,
+    title: "Elegant Bifocal Reading Glasses",
+    description: "Bifocal reading glasses with a sophisticated frame design.",
+    price: 69.99,
+    size: "Medium",
+    image: "bifocalglasses.jpg",
+  },
+  {
+    id: 11,
+    title: "Stylish Tortoise Shell Glasses",
+    description: "Trendy tortoise shell frames for a fashion-forward look.",
+    price: 109.99,
+    size: "Medium",
+    image: "tortoiseglasses.jpg",
+  },
+  {
+    id: 12,
+    title: "Sports Wraparound Sunglasses",
+    description:
+      "Wraparound sunglasses for outdoor enthusiasts with UV protection.",
+    price: 79.99,
+    size: "Large",
+    image: "wraparoundsunglasses.jpg",
+  },
+  {
+    id: 13,
+    title: "Rimless Reading Glasses",
+    description:
+      "Rimless reading glasses for a minimalist and lightweight design.",
+    price: 59.99,
+    size: "Small",
+    image: "rimlessglasses.jpg",
+  },
+  {
+    id: 14,
+    title: "Professional Pilot Sunglasses",
+    description:
+      "Pilot sunglasses with a classic aviator style and polarized lenses.",
+    price: 129.99,
+    size: "Large",
+    image: "pilotsunglasses.jpg",
+  },
+  {
+    id: 15,
+    title: "Designer Cat-Eye Frames",
+    description:
+      "Designer cat-eye frames with a luxurious design and metal accents.",
+    price: 149.99,
+    size: "Medium",
+    image: "designercateye.jpg",
+  },
+  {
+    id: 16,
+    title: "Retro Square Glasses",
+    description:
+      "Square frames with a retro look and prescription lens options.",
+    price: 69.99,
+    size: "Medium",
+    image: "retrosquare.jpg",
+  },
+  {
+    id: 17,
+    title: "Vintage Round Sunglasses",
+    description: "Round sunglasses with a vintage appeal and gradient lenses.",
+    price: 89.99,
+    size: "Medium",
+    image: "vintageround.jpg",
+  },
+  {
+    id: 18,
+    title: "Modern Metal-Frame Glasses",
+    description: "Metal-frame glasses with a modern, lightweight design.",
+    price: 79.99,
+    size: "Medium",
+    image: "metalframe.jpg",
+  },
+  {
+    id: 19,
+    title: "Geometric Shape Glasses",
+    description: "Geometric shape frames for a unique and artistic look.",
+    price: 119.99,
+    size: "Medium",
+    image: "geometricshape.jpg",
+  },
+  {
+    id: 20,
+    title: "Sports Prescription Glasses",
+    description:
+      "Prescription glasses designed for sports and outdoor activities.",
+    price: 99.99,
+    size: "Large",
+    image: "sportsprescription.jpg",
+  },
+  {
+    id: 21,
+    title: "Aviator Sunglasses with Mirrored Lenses",
+    description:
+      "Aviator sunglasses with mirrored lenses for a bold and stylish appearance.",
+    price: 109.99,
+    size: "Large",
+    image: "aviatormirrored.jpg",
+  },
+  {
+    id: 22,
+    title: "Trendy Oversized Sunglasses",
+    description:
+      "Oversized sunglasses with trendy colors and full UV protection.",
+    price: 79.99,
+    size: "Large",
+    image: "trendyoversized.jpg",
+  },
+  {
+    id: 23,
+    title: "Bamboo Wood Frames",
+    description:
+      "Eyeglasses with bamboo wood frames for an eco-friendly and unique style.",
+    price: 129.99,
+    size: "Medium",
+    image: "bamboowood.jpg",
+  },
+  {
+    id: 24,
+    title: "Fashionable Hexagonal Glasses",
+    description: "Hexagonal frames with a fashionable and geometric design.",
+    price: 119.99,
+    size: "Medium",
+    image: "hexagonalglasses.jpg",
+  },
+  {
+    id: 25,
+    title: "Sports Wraparound Sunglasses",
+    description:
+      "Wraparound sunglasses with polarized lenses and a secure fit for sports.",
+    price: 89.99,
+    size: "Large",
+    image: "sportswraparound.jpg",
+  },
+];
+
+// Your array of products
+var productss = [
+  {
+    id: 1,
+    title: "Classic Wayfarer Sunglasses",
+    description: "Timeless style with UV protection and polarized lenses.",
+    price: 99.99,
+    size: "Medium",
+    images: ["wayfarer.jpg", "wayfarer2.jpg"],
+  },
+  {
+    id: 2,
+    title: "Retro Round Glasses",
+    description: "Vintage-inspired round frames with clear lenses.",
+    price: 49.99,
+    size: "Small",
+    images: ["roundglasses.jpg", "roundglasses2.jpg"],
+  },
+  {
+    id: 3,
+    title: "Retro Round Glasses",
+    description: "Vintage-inspired round frames with clear lenses.",
+    price: 49.99,
+    size: "Small",
+    images: ["roundglasses.jpg", "roundglasses2.jpg"],
+  },
+  // Add more products here...
+];
+
+// Function to generate HTML for each product
+function generateProductHTML(product) {
+  return `
+  <div class="product mb-3">
+  <a href="#">
+    <div class="product-single-card">
+      <div class="product-top-area">
+        <div class="product-discount">
+        $${product.price}
+        </div>
+        <div class="product-img">
+          <div class="first-view">
+            <img src="${product.images[0]}" alt="${product.title}" class="product-img">
+          </div>
+          <div class="hover-view">
+            <img src="${product.images[1]}" alt="${product.title}" class="product-img">
+          </div>
+        </div>
+        <div class="sideicons">
+          <button class="sideicons-btn">
+            <i class="bi bi-cart-plus-fill"></i>
+          </button>
+          <button class="sideicons-btn">
+            <i class="bi bi-eye-fill"></i>
+          </button>
+          <button class="sideicons-btn">
+            <i class="bi bi-heart-fill"></i>
+          </button>
+          <button class="sideicons-btn">
+            <i class="bi bi-shuffle"></i>
+          </button>
+        </div>
+      </div>
+      <div class="product-info">
+        <h6 class="product-category m-0"><a href="#"
+            class="text-decoration-none text-dark m-0">${product.title}</a></h6>
+        <h6 class="product-title text-truncate"><a href="#"
+            class="text-decoration-none text-secondary">${product.description}</a></h6>
+        <div class="d-flex flex-wrap align-items-center pb-2">
+          <div class="new-price">
+          $${product.price}
+          </div>
+          <div class="old-price">
+            $50.45
+          </div>
+          <p>Size: ${product.size}</p>
+          <div class="d-flex align-items-center product-review">
+            <div class="review-star me-1">
+              <i class="bi bi-star-fill"></i>
+            </div>
+            <span class="review-count ml-1">4.5</span>
+            <span class="review-count ml-2">(134)</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </a>
+</div>
+    `;
+}
+
+// Function to insert product HTML into the page
+function displayProducts() {
+  let productListDiv = document.getElementById("productList");
+
+  productss.forEach(function (product) {
+    let productHTML = generateProductHTML(product);
+    productListDiv.innerHTML += productHTML;
+  });
+}
+
+// Call the function to display products when the page loads
+window.onload = displayProducts;
